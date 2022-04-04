e84fd57d50f3a883d15bf580e5bf433b641dc8fa
maximilianoangel <maximiliano.angel@mail.udp.cl>
2022-04-04 19:32:20 -0400

    v2
    Se arreglan diagonales

--- N-Reinas-1.py
+++ N-Reinas-1.py
@@ -1,86 +1,108 @@
 import numpy as np
 
-i=4
-
 def matriz_generator(i): #genera la matriz
 
-    matriz=np.zeros((i,i))
+    matriz=np.zeros((i,i),dtype=int)
 
 
     return matriz
 
 def summation(matriz,fila,columna,i): #suma y decide si poner o no la reina
-    aux1=0
-    aux2=0
-    aux3=5
-    aux4=0
-    posC=columna
-    posF=fila
-    var=0
-    posFF=fila
-    posCC=columna
-    n=5
-    while var<i:
-        aux1=matriz[fila][var]+aux1
-        aux2=matriz[var][columna]+aux2
-        if (fila==0 and columna==0) or (fila ==i-1 and columna==i-1):
-            aux3=matriz[var][var]+aux3
-        elif (fila==0 and columna>0 and columna<i) or (fila==i-1 and columna<i):
-            aux4=matriz[var][posC]+aux4
-            posC=posC-1
-        elif columna==0 and fila>0 and fila<i:
-            aux4=matriz[posF][var]
-            posF=posF-1
-        var=1+var
-    if columna>0 and columna<i-1 and fila >0 and fila<i-1:
-        while posFF>0 or posCC>0:
-            if posFF>0 and posFF<posCC and posCC>=0:
-                posCC=posCC-1
-                posFF=posFF-1
-                if posFF==0:
-                    aux3=int(sum(np.diag(matriz,k=posCC)))
-                    break
-            elif posFF>0 and posFF>posCC and posCC>=0:
-                posCC=posCC-1
-                posFF=posFF-1
-                if posCC==0:
-                    aux3=int(sum(np.diag(matriz,k=posFF*-1)))
-                    break
-            else:
-                break
-
-    if aux1==0 and aux2==0 and aux3==0:
+    sumatoria1 = 0
+    sumatoria2 = 0
+    sumatoria3 = 0
+    sumatoria4 = 0
+    sumatoria5 = 0
+    sumatoria6 = 0
+    contador_fila = 0
+    contador_columna = 0
+    while contador_fila < i: # suma la columna
+        sumatoria1 += matriz[contador_fila][columna]
+        contador_fila += 1
+    contador_fila = 0 
+    while contador_columna <= columna: # suma la fila
+        sumatoria2 += matriz[fila][contador_columna]
+        contador_columna += 1
+    contador_columna = columna
+    contador_fila = fila
+    while True:
+        while contador_columna >= 0 and contador_fila >= 0: # Suma esquina superior izquierda
+            sumatoria3 += matriz[contador_fila][contador_columna]
+            contador_columna -= 1
+            contador_fila -= 1
+            # print("entra 1")
+        contador_columna = columna
+        contador_fila = fila
+        while contador_columna >= 0 and contador_fila < i: # Suma esquina inferior izquierda
+            sumatoria4 += matriz[contador_fila][contador_columna]
+            contador_columna -= 1
+            contador_fila += 1
+            # print("entra 2")
+        contador_columna = columna
+        contador_fila = fila
+        while contador_columna < i and contador_fila >= 0: # Suma esquina superior derecha
+            sumatoria5 += matriz[contador_fila][contador_columna]
+            contador_columna += 1
+            contador_fila -= 1
+            # print("entra 3")
+        contador_columna = columna
+        contador_fila = fila
+        while contador_columna < i and contador_fila < i: # Suma esquina inferior derecha
+            sumatoria6 += matriz[contador_fila][contador_columna]
+            contador_columna += 1
+            contador_fila += 1
+            # print("entra 4")
+        break
+    if sumatoria6==0 and sumatoria1==0 and sumatoria2==0 and sumatoria3==0 and sumatoria4==0 and sumatoria5==0:
         return 0
-    elif aux1==1 or aux2==1 or aux3==1:
+    elif sumatoria6==1 or sumatoria1==1 or sumatoria2==1 or sumatoria3==1 or sumatoria4==1 or sumatoria5==1:
         return 1
-    else:
+    elif sumatoria6>1 or sumatoria1>1 or sumatoria2>1 or sumatoria3>1 or sumatoria4>1 or sumatoria5>1:
         return 2
 
 
-def solver(i): #hace los llamados para generar la matriz y calcular si poner o no la reina
+def solver(i,fila,columna,iteracion): #hace los llamados para generar la matriz y calcular si poner o no la reina
     matriz=matriz_generator(i)
-    aux=(i*i)+1
-    fila=0
-    columna=0
-    var=1
-    while var<aux:
+    aux=(i*i)
+    filaI=fila
+    ColumnaI=columna
+    print("iteracion " + str(iteracion))
+    while True:
+        print("fila " + str(fila))
         Mvar=summation(matriz,fila,columna,i)
         if Mvar==0:
             matriz[fila][columna]=1
+            if columna<i and fila<i:
+                print(columna)
+                columna=columna+1
         elif Mvar==1:
             if columna<i and fila<i:
+                print(columna)
                 columna=columna+1
-                Mvar=summation(matriz,fila,columna,i)
         elif Mvar==2:
             print("Error fatal garrafal")
-        if var%i==0:
+        if columna%i==0 and fila<i and columna!=0:
             fila=fila+1
             columna=0
+            if fila==i:
+                break
+    if sum(sum(matriz))==i:
+        print ("solucion!!")
+        print (matriz)
+    else:
+        if ColumnaI+1%i==0:
+            print("llegue " + str(filaI+1))
+            solver(i,filaI+1,0,iteracion+1)
         else:
-            columna=columna+1
-        var=var+1
-    print(matriz)
-    print(sum(sum(matriz)))
+            print("aca")
+            if ColumnaI+1==i:
+                ColumnaI=-1
+                filaI=filaI+1
+                print("Fila inicial " + str(filaI))
+            solver(i,filaI,ColumnaI+1,iteracion+1)
+    if iteracion==aux:
+        return matriz
+i=4
+solver(11,0,0,0)
 
-solver(i)
 
